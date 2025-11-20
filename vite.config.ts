0bbad024ccf0b62970786eaff82f5eaa397bcd05
meetsavani07 <savanimeet11@gmail.com>
2025-11-20 12:05:02 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: 'auto',
-      devOptions: {
-        enabled: true,
-      },
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
-      manifest: {
-        name: "ClearTask",
-        short_name: "ClearTask",
-        description: "App Description",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-    }),
-  ],
-});
+  plugins: [react()],
+})
